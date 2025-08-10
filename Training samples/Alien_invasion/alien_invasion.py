344430a84bebde8ec195b151dc287bacd6d0d3b7
Artem-Shabaltun <starlord1819@gmail.com>
2025-08-10 18:40:55 +0300

    ship and settings

--- Training samples/Alien_invasion/alien_invasion.py
+++ Training samples/Alien_invasion/alien_invasion.py
@@ -1,15 +1,24 @@
 import sys
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
     "загальний клас, для керування ресурсами та поведінкою"
 
     def __init__(self):
         "ініціалізувати гру, створити ресурси"
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+
+        # задаємо фон
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         "Розпочати цикл гри"
@@ -18,6 +27,10 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            #Заново перемалювати екран
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # показ останнього намальованого екрану
             pygame.display.flip()
