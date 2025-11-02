d01a2db825ac1f1a1f8c8c156b240a275ebb5e54
Artem-Shabaltun <starlord1819@gmail.com>
2025-11-02 19:55:17 +0200

    Day 5

---
+++ Training samples/100 Days of Python/5/task.py
@@ -0,0 +1,9 @@
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " pie")
+print(fruits)
+
+
+
+students_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
