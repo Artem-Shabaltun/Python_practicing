03dfe38a0f273963dcea6fbe55337a26f4b14d1a
Artem-Shabaltun <starlord1819@gmail.com>
2025-11-23 20:25:31 +0200

    Day 5
    Task 5

--- Training samples/100 Days of Python/5/task.py
+++ Training samples/100 Days of Python/5/task.py
@@ -1,9 +1,49 @@
-fruits = ["Apple", "Peach", "Pear"]
-for fruit in fruits:
-    print(fruit)
-    print(fruit + " pie")
-print(fruits)
+# fruits = ["Apple", "Peach", "Pear"]
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit + " pie")
+# print(fruits)
 
 
 
-students_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+# students_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+
+
+# total_exam_score = sum(students_scores)
+#
+# sum = 0
+# for score in students_scores:
+#     sum += score
+#
+#
+# print(sum)
+
+# print(max(students_scores))
+
+# max_score = 0
+# for score in students_scores:
+#     if score > max_score:
+#         max_score = score
+#     else:
+#         continue
+#
+# print(max_score)
+
+
+# Range function
+
+
+# sum = 0
+# for number in range (1, 101):
+#     sum += number
+# print(sum)
+
+for number in range (1, 101):
+    if number %3 == 0 and number %5 ==0:
+        print("FizzBuzz")
+    elif number % 5 ==0:
+        print("Buzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    else:
+        print(number)
