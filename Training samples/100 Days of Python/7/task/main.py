0b9e7b4a1339f80d818a501ab76655a0ec41130e
Artem-Shabaltun <starlord1819@gmail.com>
2026-01-03 20:51:34 +0200

    Day 7
    Hangman game - the base

--- Training samples/100 Days of Python/7/task/main.py
+++ Training samples/100 Days of Python/7/task/main.py
@@ -1,8 +1,96 @@
+import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''','''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''','''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''' ]
+
+
 word_list = ["aardvark", "baboon", "camel"]
+lives = 6
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
+chosen_word = random.choice(word_list)
+print(chosen_word)
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+placeholder = ""
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
+for position in range(len(chosen_word)):
+    placeholder += "_"
+print(placeholder)
+
+game_over = False
+right_letters = []
+
+while not game_over:
+    guess = input("Guess a letter: ").lower()
+
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            right_letters.append(guess)
+
+        elif letter in right_letters:
+            display += letter
+        else:
+            display += "_"
+    print(display)
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            game_over = True
+            print("You Lose!")
+
+    if "_" not in display:
+        game_over = True
+        print("You win!")
+
+    print(stages[lives])
