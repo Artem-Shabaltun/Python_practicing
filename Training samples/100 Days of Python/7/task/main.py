e6178246bd1ad7536beb10fbf326f45d014212d4
Artem-Shabaltun <starlord1819@gmail.com>
2026-01-04 15:51:20 +0200

    Day 7
    Added the .exe file for game

--- Training samples/100 Days of Python/7/task/main.py
+++ Training samples/100 Days of Python/7/task/main.py
@@ -1,10 +1,70 @@
+# import random
+# from hangman_words import word_list
+# from hangman_arts import stages, logo
+#
+# # LIVES AREA
+# lives = 6
+#
+#
+# print(logo)
+# chosen_word = random.choice(word_list)
+# # print(f"for testing: {chosen_word}")
+#
+# # PLACEHOLDERS FOR THE LETTERS
+# placeholder = ""
+#
+# for position in range(len(chosen_word)):
+#     placeholder += "_"
+# print(placeholder)
+#
+# # MAIN LOGIC
+# game_over = False
+# right_letters = []
+#
+# while not game_over:
+#     print(f"********************** {lives}/6 LIVES LEFT **********************")
+#     guess = input("Guess a letter: ").lower()
+#
+#     if guess in right_letters:
+#         print (f"You've already guessed {guess}")
+#
+#     display = ""
+#
+#     for letter in chosen_word:
+#         if letter == guess:
+#             display += letter
+#             right_letters.append(guess)
+#
+#         elif letter in right_letters:
+#             display += letter
+#         else:
+#             display += "_"
+#     print("Word to guess: " + display)
+#
+#     if guess not in chosen_word:
+#         lives -= 1
+#         print(f"{guess}, that's not in the word. You lose a life")
+#         if lives == 0:
+#             game_over = True
+#
+#             print(f"********************** {lives}/6 LIVES LEFT **********************")
+#             print(f"************* THE WORD WAS:{chosen_word}, YOU LOSE! *****************")
+#
+#     if "_" not in display:
+#         game_over = True
+#         print("********************** YOU WIN! **********************")
+#
+#     print(stages[lives])
+
+
+
+
 import random
 from hangman_words import word_list
 from hangman_arts import stages, logo
 
 # LIVES AREA
 lives = 6
-
 
 print(logo)
 chosen_word = random.choice(word_list)
@@ -26,7 +86,7 @@ while not game_over:
     guess = input("Guess a letter: ").lower()
 
     if guess in right_letters:
-        print (f"You've already guessed {guess}")
+        print(f"You've already guessed {guess}")
 
     display = ""
 
@@ -34,7 +94,6 @@ while not game_over:
         if letter == guess:
             display += letter
             right_letters.append(guess)
-
         elif letter in right_letters:
             display += letter
         else:
@@ -46,12 +105,14 @@ while not game_over:
         print(f"{guess}, that's not in the word. You lose a life")
         if lives == 0:
             game_over = True
-
             print(f"********************** {lives}/6 LIVES LEFT **********************")
-            print(f"************* THE WORD WAS:{chosen_word}, YOU LOSE! *****************")
+            print(f"************* THE WORD WAS: {chosen_word}, YOU LOSE! *****************")
 
     if "_" not in display:
         game_over = True
         print("********************** YOU WIN! **********************")
 
     print(stages[lives])
+
+input("\npush Enter, for close the game...")
+
